078562c127334ea8456c491dabc3a67b8ebd787c
jainsang <sangeeta.jain@intel.com>
2018-09-12 11:48:40 +0800

    updated test case
    Signed-off-by: jainsang <sangeeta.jain@intel.com>

--- hello_world.py
+++ hello_world.py
@@ -2,9 +2,12 @@ from oeqa.runtime.case import OERuntimeTestCase
 from oeqa.core.decorator.depends import OETestDepends
 from oeqa.core.decorator.oeid import OETestID
 
-class BspRuntimeTest(OERuntime TestCase):
+class BspRuntimeTest(OERuntimeTestCase):
 
     @OETestID(001)
     def test_print_message():
-        print "Hello World from Sangeeta"
-
+        command = (print "Hello World from Sangeeta")
+	status, output = self.target.run(command)
+        msg = ('testcase not working as expected. '
+                'Status and output:%s and %s.' % (status, output))
+        self.assertEqual(status, 0, msg = msg)
